d8ec61e4dfe96a44026a907661211bf28b5e5689
brayan martinez <56900160+brayan1234434@users.noreply.github.com>
2019-10-29 20:31:58 -0500

    Update formcalculadora.php

--- formcalculadora.php
+++ formcalculadora.php
@@ -16,7 +16,18 @@ $op=$_REQUEST['operacion'];
 switch ($op) {
 	case 1
 	break;
-	
+	case 2
+		if($_POST)
+	{	
+		$num1 = $_POST
+		['numero1'];
+		$num2 = $_POST
+		['numero2'];
+		$resta = $num1 
+		+ $num2;
+		echo "La resta de ".$num1." y ".$num2." es ".$resta; 
+	}
+	break;
 	case 5:
 		$num = 25;
 		$raiz_cuadrada = sqrt ($num);
